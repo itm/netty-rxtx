65c58936550fcb6bf59de1482198d6d647c03949
Daniel Bimschas <daniel@bimschas.com>
2011-01-20 16:55:06 +0100

    Added unit test for DleStxEtxDecoder

---
+++ dlestxetx/src/test/java/DleStxEtxFramingDecoderTest.java
@@ -0,0 +1,85 @@
+import de.uniluebeck.itm.nettyrxtx.dlestxetx.DleStxEtxConstants;
+import de.uniluebeck.itm.nettyrxtx.dlestxetx.DleStxEtxFramingDecoder;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.handler.codec.embedder.DecoderEmbedder;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class DleStxEtxFramingDecoderTest {
+
+	@Test
+	public void testPacket() {
+
+		final String payloadString = "hello, world";
+		byte[] payload = payloadString.getBytes();
+
+		ChannelBuffer encodedBuffer = ChannelBuffers.wrappedBuffer(
+				DleStxEtxConstants.DLE_STX,
+				payload,
+				DleStxEtxConstants.DLE_ETX
+		);
+
+		testInternal(encodedBuffer, payload);
+	}
+
+	@Test
+	public void testRandomBytesBeforePacket() {
+
+		final String payloadString = "hello, world";
+		byte[] expectedPayload = payloadString.getBytes();
+
+		ChannelBuffer encodedBuffer = ChannelBuffers.wrappedBuffer(
+				"klasjd".getBytes(),
+				DleStxEtxConstants.DLE_STX,
+				expectedPayload,
+				DleStxEtxConstants.DLE_ETX
+		);
+
+		testInternal(encodedBuffer, expectedPayload);
+	}
+
+	@Test
+	public void testRandomBytesAfterPacket() {
+
+		final String payloadString = "hello, world";
+		byte[] expectedPayload = payloadString.getBytes();
+
+		ChannelBuffer encodedBuffer = ChannelBuffers.wrappedBuffer(
+				DleStxEtxConstants.DLE_STX,
+				expectedPayload,
+				DleStxEtxConstants.DLE_ETX,
+				"klasjd".getBytes()
+		);
+
+		testInternal(encodedBuffer, expectedPayload);
+	}
+
+	@Test
+	public void testRandomBytesBeforeAndAfterPacket() {
+
+		final String payloadString = "hello, world";
+		byte[] expectedPayload = payloadString.getBytes();
+
+		ChannelBuffer encodedBuffer = ChannelBuffers.wrappedBuffer(
+				"klasjd".getBytes(),
+				DleStxEtxConstants.DLE_STX,
+				expectedPayload,
+				DleStxEtxConstants.DLE_ETX,
+				"klasjd".getBytes()
+		);
+
+		testInternal(encodedBuffer, expectedPayload);
+	}
+
+	private void testInternal(ChannelBuffer encodedBuffer, byte[] expectedPayload) {
+		DecoderEmbedder<ChannelBuffer> decoder = new DecoderEmbedder<ChannelBuffer>(new DleStxEtxFramingDecoder());
+		decoder.offer(encodedBuffer);
+		ChannelBuffer decodedBuffer = decoder.poll();
+		byte[] decodedPayload = new byte[decodedBuffer.readableBytes()];
+		decodedBuffer.readBytes(decodedPayload);
+		assertArrayEquals(expectedPayload, decodedPayload);
+	}
+
+}
